6e288ee97a13a1175dc06f7531233e1d0f4bc79d
Guilherme Santos <guilhermesj.dev@gmail.com>
2023-05-08 10:18:16 -0300

    Implementa delete

--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -50,7 +50,7 @@ public class ClienteApplicationService implements ClienteService {
 	public void deletaClienteAtravesId(UUID idCliente) {
 		log.info("[inicia] ClienteApplicationSevice - deletaClienteAtravesId");
 		Cliente cliente = clienteRepository.buscaClienteAtravesId(idCliente);
-	//	ClienteRepository.deletaCliente(cliente);
+		clienteRepository.deletaCliente(cliente);
 		log.info("[finaliza] ClienteApplicationSevice - deletaClienteAtravesId");
 		
 		
