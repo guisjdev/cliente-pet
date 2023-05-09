b1f841a728aac858221dc0187b39c527cf3dc603
Guilherme Santos <guilhermesj.dev@gmail.com>
2023-05-09 10:36:05 -0300

    Implementa fluxo petService buscaPet detalhado

--- src/main/java/br/com/petz/clientepet/pet/application/service/PetService.java
+++ src/main/java/br/com/petz/clientepet/pet/application/service/PetService.java
@@ -3,6 +3,7 @@ package br.com.petz.clientepet.pet.application.service;
 import java.util.List;
 import java.util.UUID;
 
+import br.com.petz.clientepet.pet.application.api.PetClienteDetalheResponse;
 import br.com.petz.clientepet.pet.application.api.PetClienteListResponse;
 import br.com.petz.clientepet.pet.application.api.PetRequest;
 import br.com.petz.clientepet.pet.application.api.PetResponse;
@@ -11,5 +12,6 @@ import jakarta.validation.Valid;
 public interface PetService {
 	PetResponse criaPet(UUID idCliente, @Valid PetRequest petRequest);
 	List<PetClienteListResponse> buscaPetsDoClienteComId(UUID idCliente);
+	PetClienteDetalheResponse buscaPetsDoClienteComId(UUID idCliente, UUID idPet);
 
 }
