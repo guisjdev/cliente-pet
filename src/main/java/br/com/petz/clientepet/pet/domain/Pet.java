01e752111ffd7e245f3625ddb03d23cd5780a7dd
Guilherme Santos <guilhermesj.dev@gmail.com>
2023-05-08 22:53:42 -0300

    implementa save pet

--- src/main/java/br/com/petz/clientepet/pet/domain/Pet.java
+++ src/main/java/br/com/petz/clientepet/pet/domain/Pet.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import br.com.petz.clientepet.pet.application.api.PetRequest;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -11,6 +12,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -25,6 +27,8 @@ public class Pet {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id", updatable = false, unique = true, nullable = false)
 	private UUID idPet;
+	@NotNull
+	private UUID idClienteTutor;
 	@NotBlank
 	private String nomePet;
 	@Enumerated(EnumType.STRING)
@@ -41,6 +45,22 @@ public class Pet {
 	private String rga;
 	private Integer peso;
 
+	
 	private LocalDateTime dataHoraDoCadastro;
 	private LocalDateTime dataHoraDaUltimaAlteracao;
+
+	public Pet(UUID idCliente, @Valid PetRequest petRequest) {
+		this.idClienteTutor = idCliente;
+		this.nomePet = petRequest.getNomePet();
+		this.porte = petRequest.getPorte();
+		this.tipo = petRequest.getTipo();
+		this.microchip = petRequest.getMicrochip();
+		this.raca = petRequest.getRaca();
+		this.sexo = petRequest.getSexo();
+		this.pelagemCor = petRequest.getPelagemCor();
+		this.dataNascimento = petRequest.getDataNascimento();
+		this.rga = petRequest.getRga();
+		this.peso = petRequest.getPeso();
+		this.dataHoraDoCadastro =LocalDateTime.now();
+	}
 }
